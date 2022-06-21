2d716adc174171367bf3ddfb379e7260940c7c97
vinicius <vinicius.lima248@etec.sp.gov.br>
2022-06-21 08:55:39 -0300

    tcc 2.1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { CadastroComponent } from './telas/cadastro/cadastro.component';
+import { LoginComponent } from './telas/login/login.component';
+import { HomeComponent } from './template/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'cadastro', component: CadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
